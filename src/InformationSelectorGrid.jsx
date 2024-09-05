a8404d2818951e8ae019078154cc628eb2d6b6c6
MichelleCordovi <michellecordovi@gmail.com>
2024-09-05 11:47:43 -0400

    create logic for information selector grid to change description provided

--- src/InformationSelectorGrid.jsx
+++ src/InformationSelectorGrid.jsx
@@ -1,17 +1,35 @@
-function InformationSelectorGrid(){
+/* eslint-disable react/prop-types */
+function InformationSelectorGrid({setSelectedInformation}){
+
+    function handleClick(event){
+        if(event.currentTarget.classList.contains("overview")){
+            setSelectedInformation(() => {
+                return "overview"
+            })
+        } else if(event.currentTarget.classList.contains("structure")){
+            setSelectedInformation(() => {
+                return "structure"
+            })
+        } else if(event.currentTarget.classList.contains("geology")){
+            setSelectedInformation(() => {
+                return "geology"
+            })
+        }
+    }
+    
     return (
         <div id="information-selector-grid">
-            <div className="information-selector-box">
+            <div onClick={handleClick} className="information-selector-box overview">
                 <p className="selector-number">01</p>
                 <h2>Overview</h2>
             </div>
 
-            <div className="information-selector-box">
+            <div onClick={handleClick} className="information-selector-box structure">
                 <p className="selector-number">02</p>
                 <h2>Internal Structure</h2>
             </div>
 
-            <div className="information-selector-box">
+            <div onClick={handleClick} className="information-selector-box geology">
                 <p className="selector-number">03</p>
                 <h2>Surface Geology</h2>
             </div>
--- src/components/PlanetPage.jsx
+++ src/components/PlanetPage.jsx
@@ -1,11 +1,32 @@
 /* eslint-disable react/prop-types */
+import {useState, useEffect} from 'react'
 import {useParams} from 'react-router-dom'
 import PlanetDataGrid from './PlanetDataGrid'
 import InformationSelectorGrid from '../InformationSelectorGrid'
 
 function PlanetPage({planets}) {
+    const [selectedInformation, setSelectedInformation] = useState("overview")
     const {planet} = useParams()
     const planetData = planets.find(p => p.name.toLowerCase() === planet.toLowerCase())
+
+    useEffect(() => {
+        console.log(selectedInformation)
+    },[selectedInformation])
+
+
+    //function to provide planet description based on selected information
+    function getPlanetDescription() {
+        switch (selectedInformation) {
+            case "overview":
+                return planetData.overview.content;
+            case "structure":
+                return planetData.structure.content;
+            case "geology":
+                return planetData.geology.content;
+            default:
+                return planetData.overview.content;
+        }
+    }
 
     return (
         <main>
@@ -16,9 +37,9 @@ function PlanetPage({planets}) {
 
                 <div id="description-block">
                     <h1>{planetData.name}</h1>
-                    <p id="description">{planetData.overview.content}</p>
+                    <p id="description">{getPlanetDescription()}</p>
                     <p id="source">Source: <span className="source-link">WikiPedia</span></p>
-                    <InformationSelectorGrid/>
+                    <InformationSelectorGrid setSelectedInformation={setSelectedInformation} />
                 </div>
             </section>
             
